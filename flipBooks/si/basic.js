3a2118bc08354f50750a0d90027f240da6e2778e
chisNan <artgreg@outlook.fr>
2021-09-18 17:59:01 +0200

    - update flipbook si

--- flipBooks/si/basic.js
+++ flipBooks/si/basic.js
@@ -1,6 +1,9 @@
 (function($) {
 
-var if_you_should_be = "<p class='t'><u>Si vous deviez être...</u></p><p>Un musicien: <a href='https://en.wikipedia.org/wiki/Mizell_Brothers' target='_blank'>The Mizell Brothers</a></p><p>Un lieu: <a href='https://www.google.fr/maps/place/Reserva+Natural+Tanimboca/@-4.1179155,-69.9525982,12z/data=!4m5!3m4!1s0x0:0x605077e25303b603!8m2!3d-4.1179155!4d-69.9525982' target='_blank'>Kilómetro 11 de Leticia</a></p><p>Un film: <a href='https://en.wikipedia.org/wiki/One_Flew_Over_the_Cuckoo%27s_Nest_(film)' target='_blank'>One flew over the cuckoo's nest</a></p><p>Un livre: <a href='https://fr.wikipedia.org/wiki/Les_Mains_du_miracle' target='_blank'>Les mains du miracle</a></p><p>Une citation: <br>'​Les souffrances doivent nous être supportables par la raison que c’est nous-même qui nous les infligeons et que nous souffrons que dans la mesure où nous coopérons à nos souffrances. (Novalis)'</p>";
+var if_you_should_be = `<p class='t'><u>Si vous deviez être...</u></p><p>Un musicien: <a href='https://shop.hammockmusic.com' target='_blank'>Hammock</a></p>
+<p>Un lieu: <a href='https://www.google.com/maps/dir//Thekkady,+Kumily,+Kerala,+Inde/@9.6093969,77.1605009,14z/data=!4m17!1m7!3m6!1s0x3b06f8812df2a199:0x468af17958c54ae8!2sThekkady,+Kumily,+Kerala,+Inde!3b1!8m2!3d9.6031088!4d77.161458!4m8!1m0!1m5!1m1!1s0x3b06f8812df2a199:0x468af17958c54ae8!2m2!1d77.161458!2d9.6031088!3e2?hl=fr' target='_blank'>Thekkady</a></p>
+<p>Un film: <a href='https://en.wikipedia.org/wiki/Donnie_Darko' target='_blank'>Donnie Darko</a></p>
+<p>Un livre: <a href='https://books.google.fr/books/about/Corps_et_%C3%A2me_L_enfant_prodige.html?id=E4dCDwAAQBAJ&source=kp_book_description&redir_esc=y' target='_blank'>Corps et âme (l'enfant prodige)</a></p><p>Une citation: <br>'​Les souffrances doivent nous être supportables par la raison que c’est nous-même qui nous les infligeons et que nous souffrons que dans la mesure où nous coopérons à nos souffrances. (Novalis)'</p>`;
     var container = $('<div />', {css: {margin: 'auto', width: 1300, height: '95%'} }).appendTo($('#sample-viewer'));
 
 	$('<div />', {'class': 'sample-flipbook'})
